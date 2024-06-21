388d6546854de88fc9faa0d51a83afc710482118
flyyy26 <rfchannel0@gmail.com>
2024-06-21 16:18:42 +0700

    Download Page

---
+++ pages/download/index.js
@@ -0,0 +1,23 @@
+import RandomProduct from "@/components/random-product";
+import { Link } from "react-router-dom";
+
+const Download = () => {
+    return(
+        <>
+            <div className="download-page">
+                <div className="download-page-layouting">
+                    <h1>Scan QR dibawah untuk mendownload Aplikasi</h1>
+                    <div className="download-page-qris">
+                        <img src="/images/unduh-qr.png" alt="Hallo Beauty" />
+                    </div>
+                    <span>Atau</span>
+                    <a hreflang="/download-file"><button>Download disini</button></a>
+                </div>
+                <RandomProduct/>
+            </div>
+            
+        </>
+    );
+}
+
+export default Download
